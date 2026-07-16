f7b5e4920bd1a48aaf1c2e8b25be090b5fd5a345
Arvind Parmar <arvind1057956@gmail.com>
2026-07-16 11:55:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.56%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -29,3 +29,38 @@ public:
 
     }
 };
+
+
+// class Solution {
+// public:
+//     int myAtoi(string s) {
+//         long long result = 0;
+//         bool flag = 0;     // number started
+//         bool signSeen = 0; // sign seen
+//         int sign = 1;      // default positive
+
+//         for (int i = 0; i < s.size(); i++) {
+//             if (s[i] == ' ' && flag == 0 && signSeen == 0) continue;
+
+//             if (flag == 0 && signSeen == 0 && (s[i] == '-' || s[i] == '+')) {
+//                 signSeen = 1;
+//                 if (s[i] == '-') sign = -1;
+//                 continue;
+//             }
+
+//             int value = s[i] - '0';
+//             if (value >= 0 && value < 10) {
+//                 flag = 1;
+//                 result = result * 10 + value;
+
+//                 // Check bounds with sign applied
+//                 if (sign * result < INT_MIN) return INT_MIN;
+//                 if (sign * result > INT_MAX) return INT_MAX;
+//             } else {
+//                 break;
+//             }
+//         }
+
+//         return (int)(sign * result);
+//     }
+// };
