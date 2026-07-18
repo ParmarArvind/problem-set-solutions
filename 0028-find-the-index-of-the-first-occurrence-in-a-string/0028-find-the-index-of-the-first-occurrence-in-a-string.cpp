a54b785ebe0390ce9b27705a9c23641eefff0756
Arvind Parmar <arvind1057956@gmail.com>
2026-07-18 23:12:46 +0530

    Time: 0 ms (100%), Space: 9.5 MB (8.13%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -52,6 +52,34 @@ class Solution {
         }
         
         return -1;
-        
     }
 };
+
+// class Solution {
+// public:
+//     int strStr(string haystack, string needle) {
+
+
+//         int n=haystack.size();
+//         int m=needle.size();
+//         if(m>n) return -1;
+
+//         for(int i=0;i<=n-m;i++)
+//         {
+//             int k=i;
+//             bool isOkay=true;
+//             for(int j=0;j<m;j++)
+//             {
+//                 if(haystack[k]!=needle[j])
+//                 {
+//                     isOkay=false;
+//                     break;
+//                 }
+//                 k++;
+//             }
+//             if(isOkay) return i;
+//         }
+
+//         return -1;
+//     }
+// };
