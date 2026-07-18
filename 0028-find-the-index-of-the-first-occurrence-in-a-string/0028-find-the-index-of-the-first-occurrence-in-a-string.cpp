4f14610676037545a15911873525b93268dc727e
Arvind Parmar <arvind1057956@gmail.com>
2026-07-18 23:12:17 +0530

    Time: 2 ms (23.25%), Space: 9.4 MB (16.83%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,28 +1,57 @@
+
 class Solution {
-public:
-    int strStr(string haystack, string needle) {
-
-
-        int n=haystack.size();
-        int m=needle.size();
-        if(m>n) return -1;
-
-        for(int i=0;i<=n-m;i++)
+  public:
+    void findLPS(string &needle  ,vector<int>&lps)
+    {
+        int m=needle .size();
+        int left=0;
+        int right=1;
+        while(right<m)
         {
-            int k=i;
-            bool isOkay=true;
-            for(int j=0;j<m;j++)
+            if(needle [left]==needle [right])
             {
-                if(haystack[k]!=needle[j])
-                {
-                    isOkay=false;
-                    break;
-                }
-                k++;
+                lps[right]=left+1;
+                right++;
+                left++;
             }
-            if(isOkay) return i;
+            else{
+                if(left==0) right++;
+                else {
+                    left=lps[left-1];
+                }
+            }
         }
-
+    }
+    int strStr(string haystack, string needle ) {
+        // code here
+        int n=haystack.size();
+        int m=needle .size();
+        
+        vector<int>lps(m,0);
+        findLPS(needle ,lps);
+        
+        int first=0;
+        int second=0;
+        
+        while(first<n )
+        {
+            if(haystack[first]==needle [second])
+            {
+                second++;
+                first++;
+            }
+            else{
+                if(second==0) first++;
+                else  second=lps[second-1];
+            }
+            
+            if(second==m)
+            {
+                return first-second;
+            }
+        }
+        
         return -1;
+        
     }
 };
