67fbf7b580d1a501d80b6f3166a8f66bae6a0d78
Arvind Parmar <arvind1057956@gmail.com>
2026-07-19 00:43:15 +0530

    Time: 0 ms (100%), Space: 16.2 MB (5.26%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -1,39 +1,59 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(!head || !head->next) return head;
+        ListNode* evenDummy=new ListNode(0);
+        ListNode* oddDummy=new ListNode(0);
+        ListNode* temp=head;
+        ListNode* even=evenDummy;
+        ListNode* odd=oddDummy;
+        int i=0;
+        while(temp)
+        {
+            if(i%2==0) {
+                even->next=new ListNode(temp->val);
+                even=even->next;
+            }
+            else{
+                odd->next=new ListNode(temp->val);
+                odd=odd->next;
+            }
 
-       vector<int>arr;
-       ListNode* temp=head;
-       while(temp)
-       {
-        arr.push_back(temp->val);
-        temp=temp->next;
-       } 
+            i++;
+            temp=temp->next;
+        }
+        even->next=oddDummy->next;
 
-       temp=head;
-       for(int i=0;i<arr.size();i+=2)
-       {
-        temp->val=arr[i];
-        temp=temp->next;
-       }
-
-       for(int i=1;i<arr.size();i+=2)
-       {
-        temp->val=arr[i];
-        temp=temp->next;
-       }
-
-       return head;
+    return evenDummy->next;
     }
 };
+
+
+// class Solution {
+// public:
+//     ListNode* oddEvenList(ListNode* head) {
+//         if(!head || !head->next) return head;
+
+//        vector<int>arr;
+//        ListNode* temp=head;
+//        while(temp)
+//        {
+//         arr.push_back(temp->val);
+//         temp=temp->next;
+//        } 
+
+//        temp=head;
+//        for(int i=0;i<arr.size();i+=2)
+//        {
+//         temp->val=arr[i];
+//         temp=temp->next;
+//        }
+
+//        for(int i=1;i<arr.size();i+=2)
+//        {
+//         temp->val=arr[i];
+//         temp=temp->next;
+//        }
+
+//        return head;
+//     }
+// };
