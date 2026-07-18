aa8faf946438927a23bb1dc19ffd3c1ebed33116
Arvind Parmar <arvind1057956@gmail.com>
2026-07-19 02:11:30 +0530

    Time: 0 ms (100%), Space: 23.2 MB (98.09%) - LeetHub

--- 0382-linked-list-random-node/0382-linked-list-random-node.cpp
+++ 0382-linked-list-random-node/0382-linked-list-random-node.cpp
@@ -1,23 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+ // method -2  Reservoir Sampling
 class Solution {
 public:
-    vector<int> arr;
-    int n;
-
+    ListNode* Head;
     Solution(ListNode* head) {
-        ListNode* temp = head;
-
-        while (temp) {
-            arr.push_back(temp->val);
-            temp = temp->next;
+        Head=head;
+    }
+    
+    int getRandom() {
+        int count=1;
+        int result=0;
+        ListNode* temp=Head;
+        while(temp)
+        {
+            if(rand()%count < 1.0/count)
+            {
+                result=temp->val;
+            }
+            temp=temp->next;
+            count++;
         }
 
-        n = arr.size();
-
-        srand(time(0));      // Seed only once
-    }
-
-    int getRandom() {
-        int idx = rand() % n;
-        return arr[idx];
+        return result;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> arr;
+//     int n;
+
+//     Solution(ListNode* head) {
+//         ListNode* temp = head;
+
+//         while (temp) {
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//         }
+
+//         n = arr.size();
+
+//         srand(time(0));      // Seed only once
+//     }
+
+//     int getRandom() {
+//         int idx = rand() % n;
+//         return arr[idx];
+//     }
+// };
