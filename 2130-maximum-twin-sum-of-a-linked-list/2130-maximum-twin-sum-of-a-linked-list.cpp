b33bd61abf8b997f66f8b8487dd22c52400b6792
Arvind Parmar <arvind1057956@gmail.com>
2026-07-19 17:47:22 +0530

    Time: 7 ms (32.21%), Space: 131.2 MB (43.24%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -1,33 +1,55 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// method -2 using stack
 class Solution {
 public:
     int pairSum(ListNode* head) {
-        ListNode* temp=head;
-        vector<int>arr;
-        while(temp)
+
+        ListNode* slow=head;
+        ListNode* fast=head;
+        stack<int>st;
+
+        while(fast && fast->next)
         {
-            arr.push_back(temp->val);
-            temp=temp->next;
+            st.push(slow->val);
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        int i=0;
-        int j=arr.size()-1;
+        // now remove the st top and add it to the current nodes val i.e. its twin pair 
         int result=0;
-        while(i<j)
+        while(slow)
         {
-            result=max(result,arr[i]+arr[j]);
-            i++;
-            j--;
-        }
+            int currSum=st.top()+slow->val;
+            st.pop();
+            result=max(result,currSum);
+
+            slow=slow->next;
+        }        
 
         return result;
+
     }
 };
+
+// method -1
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         ListNode* temp=head;
+//         vector<int>arr;
+//         while(temp)
+//         {
+//             arr.push_back(temp->val);
+//             temp=temp->next;
+//         }
+//         int i=0;
+//         int j=arr.size()-1;
+//         int result=0;
+//         while(i<j)
+//         {
+//             result=max(result,arr[i]+arr[j]);
+//             i++;
+//             j--;
+//         }
+
+//         return result;
+//     }
+// };
