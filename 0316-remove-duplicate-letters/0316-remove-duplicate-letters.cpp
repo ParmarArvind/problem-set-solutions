3cd12bfd3626004ec653868f7c868144e749424c
Arvind Parmar <arvind1057956@gmail.com>
2026-07-20 02:05:14 +0530

    Time: 0 ms (100%), Space: 9.2 MB (68.82%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+       
+        int n = s.size();
+        vector<int>lastIdx(26);
+        vector<bool>visited(26,false);
+        for (int i = 0; i < n; i++)
+        {
+           lastIdx[s[i]-'a']=i;
+        }
+
+        string result;
+        int i=0;
+
+        while (i < n)
+        {
+            if(visited[s[i]-'a']) {
+                i++;
+                continue;
+            }
+
+            while(!result.empty() && result.back()>s[i] && lastIdx[result.back()-'a'] >i) 
+            {
+                visited[result.back()-'a']=false;
+                result.pop_back();
+            }
+
+            result.push_back(s[i]);
+            visited[s[i]-'a']=true;
+            i++;
+        }
+
+        return result;
+    }
+};
