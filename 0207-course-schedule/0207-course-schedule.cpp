a397d243acfed3bb331ca942c211a227f43a101e
Arvind Parmar <arvind1057956@gmail.com>
2026-07-21 03:12:11 +0530

    Time: 7 ms (45.19%), Space: 19.9 MB (38.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,6 +1,7 @@
+// method -2 using dfs
 class Solution {
 public:
-// using dfs
+
     bool dfs(unordered_map<int,vector<int>>& adj,vector<bool> &visited,vector<bool>& inRecursion,int u)
     {
         visited[u]=true;
@@ -35,8 +36,52 @@ public:
             if(!visited[i] && dfs(adj,visited,inRecursion,i) ) return false;
         }
 
-        
         return true;
-        
     }
 };
+
+// // method -1 BFS
+// class Solution {
+//   public:
+//     int canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         // code here
+//         vector<int>inDegree(numCourses,0);
+//         unordered_map<int,vector<int>>adj;
+//         for(auto &edge :prerequisites)
+//         {
+//             int v=edge[0];
+//             int u=edge[1];
+//             adj[u].push_back(v);
+//             inDegree[v]++;
+//         }
+        
+//         queue<int>que;
+//         int count=0;
+//         for(int i=0;i<numCourses;i++) 
+//         {
+//             if(inDegree[i]==0) {
+//                 que.push(i);
+//                 count++;
+//             }
+//         }
+        
+//         while(!que.empty())
+//         {
+//             int u=que.front();
+//             que.pop();
+            
+//             for(auto & v:adj[u])
+//             {
+//                 inDegree[v]--;
+                    
+//                 if(inDegree[v]==0)
+//                 {
+//                     que.push(v);
+//                     count++;
+//                 }
+//             }
+//         }
+        
+//         return count==numCourses;
+//     }
+// };
