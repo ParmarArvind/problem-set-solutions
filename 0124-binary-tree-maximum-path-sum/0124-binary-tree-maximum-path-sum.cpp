8b0f63fa78f5fcdd39146e5f94a2bc5739500510
Arvind Parmar <arvind1057956@gmail.com>
2026-07-25 16:54:50 +0530

    Time: 0 ms (100%), Space: 27.9 MB (74.88%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -9,37 +9,67 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+ //clearner code 
+ class Solution {
 public:
     int result;
+
     int solve(TreeNode* root)
     {
-        
-        int left=INT_MIN;
-        if(root->left) left=solve(root->left);
+        if(root == nullptr)
+            return 0;
 
-        int right=INT_MIN;
-        if(root->right) right=solve(root->right);
+        int left = max(0, solve(root->left));
+        int right = max(0, solve(root->right));
 
+        result = max(result, root->val + left + right);
 
-        result=max({result , root->val, 
-                    root->val +(right==INT_MIN? 0: right),
-                    root->val +(left==INT_MIN? 0: left ),
-                    root->val +(right==INT_MIN? 0: right) + (left==INT_MIN? 0: left)
-                    });
-
-
-        return max({ root->val ,
-                    root->val +(right==INT_MIN? 0: right),
-                    root->val +(left==INT_MIN? 0: left )
-                    });
+        return root->val + max(left, right);
     }
-
 
     int maxPathSum(TreeNode* root) {
-      result=INT_MIN;
 
-       solve(root);
-       return result;
+        result = INT_MIN;
+
+        solve(root);
+
+        return result;
     }
 };
+
+
+// class Solution {
+// public:
+//     int result;
+//     int solve(TreeNode* root)
+//     {
+        
+//         int left=INT_MIN;
+//         if(root->left) left=solve(root->left);
+
+//         int right=INT_MIN;
+//         if(root->right) right=solve(root->right);
+
+
+//         result=max({result , root->val, 
+//                     root->val +(right==INT_MIN? 0: right),
+//                     root->val +(left==INT_MIN? 0: left ),
+//                     root->val +(right==INT_MIN? 0: right) + (left==INT_MIN? 0: left)
+//                     });
+
+
+//         return max({ root->val ,
+//                     root->val +(right==INT_MIN? 0: right),
+//                     root->val +(left==INT_MIN? 0: left )
+//                     });
+//     }
+
+
+//     int maxPathSum(TreeNode* root) {
+//       result=INT_MIN;
+
+//        solve(root);
+//        return result;
+//     }
+// };
