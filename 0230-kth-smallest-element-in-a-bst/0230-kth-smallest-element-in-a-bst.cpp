c4e41db51f2b8a480d277faa88bd2f2d14b0b804
Arvind Parmar <arvind1057956@gmail.com>
2026-07-31 03:41:10 +0530

    Time: 0 ms (100%), Space: 24.2 MB (90.85%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -9,20 +9,46 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ // method -2
 class Solution {
 public:
-    // method 1 
-    void inorder(TreeNode* root,vector<int>&temp)
+    void inorder(TreeNode* root, int &k, int &ans)
     {
-        if(!root) return ;
-        inorder(root->left,temp);
-        temp.push_back(root->val);
-        inorder(root->right ,temp);
+        if(!root) return;
+        inorder(root->left, k, ans);
+
+        k--;
+        if(k == 0)
+        {
+            ans = root->val;
+            return;
+        }
+        inorder(root->right, k, ans);
     }
+
     int kthSmallest(TreeNode* root, int k) {
-        vector<int>temp;
-        inorder(root,temp);
-        
-        return temp[k-1];
+        int ans = -1;
+        inorder(root, k, ans);
+        return ans;
     }
 };
+
+
+ 
+// class Solution {
+// public:
+//     // method 1 
+//     void inorder(TreeNode* root,vector<int>&temp)
+//     {
+//         if(!root) return ;
+//         inorder(root->left,temp);
+//         temp.push_back(root->val);
+//         inorder(root->right ,temp);
+//     }
+//     int kthSmallest(TreeNode* root, int k) {
+//         vector<int>temp;
+//         inorder(root,temp);
+        
+//         return temp[k-1];
+//     }
+// };
