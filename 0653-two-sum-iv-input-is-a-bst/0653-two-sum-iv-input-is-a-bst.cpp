11b6ff66d723776632ecfe384475aba882095529
Arvind Parmar <arvind1057956@gmail.com>
2026-07-31 22:13:58 +0530

    Time: 0 ms (100%), Space: 36.4 MB (98.88%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,25 +11,70 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>&nums)
+    bool result;
+    TreeNode* Root;
+
+    bool isAvailable(TreeNode* root, long target)
     {
-        if(!root) return ;
-        inorder(root->left,nums);
-        nums.push_back(root->val);
-        inorder(root->right,nums);
+        if(!root) return false;
+
+        if((long)root->val == target) return true;
+
+        if((long)root->val < target) return isAvailable(root->right, target);
+        
+        return isAvailable(root->left, target);
     }
-    bool findTarget(TreeNode* root, int k) {
-        vector<int>nums;
-        inorder(root,nums);
-        int i=0;
-        int j=nums.size()-1;
-        while(i<j)
+
+    void inorder(TreeNode* curr, int k)
+    {
+        if(!curr) return;
+        if(result) return;
+
+        inorder(curr->left, k);
+
+        long need = (long)k - curr->val;
+        if(curr->val != need && isAvailable(Root, need))
         {
-            if((long long)(nums[i]+nums[j])==1LL*k) return true;
-            else if((long long)(nums[i]+nums[j])>1LL*k) j--;
-            else i++;
+            result = true;
+            return;
         }
 
-        return false;
+        inorder(curr->right, k);
+    }
+
+    bool findTarget(TreeNode* root, int k)
+    {
+        result = false;
+        Root = root;
+
+        inorder(root, k);
+
+        return result;
     }
 };
+
+//  // method -1
+// class Solution {
+// public:
+//     void inorder(TreeNode* root,vector<int>&nums)
+//     {
+//         if(!root) return ;
+//         inorder(root->left,nums);
+//         nums.push_back(root->val);
+//         inorder(root->right,nums);
+//     }
+//     bool findTarget(TreeNode* root, int k) {
+//         vector<int>nums;
+//         inorder(root,nums);
+//         int i=0;
+//         int j=nums.size()-1;
+//         while(i<j)
+//         {
+//             if((long long)(nums[i]+nums[j])==1LL*k) return true;
+//             else if((long long)(nums[i]+nums[j])>1LL*k) j--;
+//             else i++;
+//         }
+//
+//         return false;
+//     }
+// };
