52d5c5004c529c257d245227b34907e948bbdf71
Arvind Parmar <arvind1057956@gmail.com>
2026-08-19 01:58:07 +0530

    Time: 0 ms (100%), Space: 29.3 MB (56.36%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -1,35 +1,68 @@
 class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(n==k ) return *max_element(nums.begin() ,nums.end());
-        int result = -1;
-
+        int n = nums.size();
         
-        if(k==1) {
-            unordered_map<int,int>mp;
-            for(auto num:nums) mp[num]++;
-
-            for(auto &it:mp)
-            {
-                if(it.second==1) result=max(result,it.first);
-            }
-
-            return result;
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
         }
-        bool isUnique1 =false ;
-        bool isUnique2=false;
-        for(int i=1;i<n-1;i++) 
-        {
-            if(nums[0]==nums[i] ) isUnique1=true;
+       
+        unordered_map<int, int> mp;
+        for (int num : nums) {
+            mp[num]++;
+        }
+        
+        int max_val = -1;
+
+        if (k == 1) {
             
-            if(nums[n-1]==nums[i]) isUnique2=true;
+            for (int num : nums) {
+                if (mp[num] == 1) {
+                    max_val = max(max_val, num);
+                }
+            }
+            return max_val;
         }
-        if(!isUnique1) result=max(result,nums[0]);
-        if(!isUnique2) result=max(result,nums[n-1]);
         
-        if(nums[0]==nums[n-1] ) return -1;
-        return result;
-
+        if (mp[nums[0]] == 1) max_val = max(max_val, nums.front());
+        if (mp[nums[n-1]] == 1)  max_val =max(max_val, nums[n-1]);
+        
+        return max_val;
     }
 };
+
+// class Solution {
+// public:
+//     int largestInteger(vector<int>& nums, int k) {
+//         int n=nums.size();
+//         if(n==k ) return *max_element(nums.begin() ,nums.end());
+//         int result = -1;
+
+        
+//         if(k==1) {
+//             unordered_map<int,int>mp;
+//             for(auto num:nums) mp[num]++;
+
+//             for(auto &it:mp)
+//             {
+//                 if(it.second==1) result=max(result,it.first);
+//             }
+
+//             return result;
+//         }
+//         bool isUnique1 =false ;
+//         bool isUnique2=false;
+//         for(int i=1;i<n-1;i++) 
+//         {
+//             if(nums[0]==nums[i] ) isUnique1=true;
+            
+//             if(nums[n-1]==nums[i]) isUnique2=true;
+//         }
+//         if(!isUnique1) result=max(result,nums[0]);
+//         if(!isUnique2) result=max(result,nums[n-1]);
+        
+//         if(nums[0]==nums[n-1] ) return -1;
+//         return result;
+
+//     }
+// };
