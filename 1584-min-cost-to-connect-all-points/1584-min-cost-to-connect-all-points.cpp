e06ef7995c0675d5f075f276b06e5c2ac52e3356
Arvind Parmar <arvind1057956@gmail.com>
2026-08-20 02:49:28 +0530

    Time: 732 ms (23.06%), Space: 180.8 MB (13.66%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,57 +1,142 @@
+// method -2 kruskal's algorithm
 class Solution {
 public:
+
+    vector<int>rank;
+    vector<int>parent;
+    int find(int x)
+    {
+        if(x==parent[x]) return x;
+        return parent[x]=find(parent[x]);
+    }
+    void Union(int x,int y)
+    {
+        int xParent=find(x);
+        int yParent=find(y);
+        if(xParent==yParent) return;
+        
+        if(rank[xParent]<rank[yParent])
+        {
+            parent[xParent]=yParent;
+        }
+        else if(rank[yParent]<rank[xParent])
+        {
+            parent[yParent]=xParent;
+        }
+        else {
+            parent[xParent]=yParent;
+            rank[yParent]++;
+        }
+    }
+    int kruskalAlgo(vector<vector<int>>& edges)
+    {
+        int sum=0;
+        for(auto &e:edges)
+        {
+            int u=e[0];
+            int v=e[1];
+            int w=e[2];
+            
+            int uParent=find(u);
+            int vParent=find(v);
+            if(uParent==vParent) continue; //connected
+            // not connected , so connect them
+            Union(u,v);
+            // add to sum
+            sum+=w;
+        }
+        
+        return sum;
+    }
     int minCostConnectPoints(vector<vector<int>>& points) {
         int n=points.size();
         //adj list creation
-        unordered_map<int,vector<pair<int,int>>>adj;
+        vector<vector<int>>edges;;
         for(int i=0;i<n;i++)
         {
             int x1=points[i][0];
             int y1=points[i][1];
+            vector<int>temp;
             for(int j=i+1;j<n;j++)
             {
                 int x2=points[j][0];
                 int y2=points[j][1];
 
                 int wt=abs(x1-x2)+abs(y1-y2);
-                adj[i].push_back({j,wt});
-                adj[j].push_back({i,wt});
+                temp={i,j,wt};
+                edges.push_back(temp);
             }
         }
 
-        // prim's algorithm
-        int sum=0;
-        vector<int>visited(n,false);
+        auto lambda=[&](auto &a ,auto &b){
+            return a[2]<b[2];
+        };
+        sort(edges.begin() ,edges.end(),lambda);
         
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>>pq;
+        rank.resize(n,1);
+        parent.resize(n);
+        for(int i=0;i<n;i++) parent[i]=i;
         
-        pq.push({0,0});
-        
-        while(!pq.empty())
-        {
-            int wt=pq.top().first;
-            int u=pq.top().second;
-    
-            pq.pop();
-            
-            if(!visited[u])
-            {
-                visited[u]=true;
-                sum+=wt;
-                
-                for(auto &it:adj[u])
-                {
-                    int v=it.first;
-                    int w=it.second;
-                    
-                    pq.push({w,v});
-                }
-            }
-        }
+        return kruskalAlgo(edges);
 
-        return sum;
     }
 };
+
+// // method -1  prim's algorithm   TC=(Elog(E))
+// class Solution {
+// public:
+//     int minCostConnectPoints(vector<vector<int>>& points) {
+//         int n=points.size();
+//         //adj list creation
+//         unordered_map<int,vector<pair<int,int>>>adj;
+//         for(int i=0;i<n;i++)
+//         {
+//             int x1=points[i][0];
+//             int y1=points[i][1];
+//             for(int j=i+1;j<n;j++)
+//             {
+//                 int x2=points[j][0];
+//                 int y2=points[j][1];
+
+//                 int wt=abs(x1-x2)+abs(y1-y2);
+//                 adj[i].push_back({j,wt});
+//                 adj[j].push_back({i,wt});
+//             }
+//         }
+
+//         // prim's algorithm
+//         int sum=0;
+//         vector<int>visited(n,false);
+        
+//         priority_queue<
+//             pair<int,int>,
+//             vector<pair<int,int>>,
+//             greater<pair<int,int>>>pq;
+        
+//         pq.push({0,0});
+        
+//         while(!pq.empty())
+//         {
+//             int wt=pq.top().first;
+//             int u=pq.top().second;
+    
+//             pq.pop();
+            
+//             if(!visited[u])
+//             {
+//                 visited[u]=true;
+//                 sum+=wt;
+                
+//                 for(auto &it:adj[u])
+//                 {
+//                     int v=it.first;
+//                     int w=it.second;
+                    
+//                     pq.push({w,v});
+//                 }
+//             }
+//         }
+
+//         return sum;
+//     }
+// };
