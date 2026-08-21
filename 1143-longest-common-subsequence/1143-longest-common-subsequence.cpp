7ecd0ef7b52ab1d622014c573704dcfd99cdff62
Arvind Parmar <arvind1057956@gmail.com>
2026-08-22 02:19:53 +0530

    Time: 27 ms (57.71%), Space: 27.3 MB (70.87%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -2,36 +2,39 @@ class Solution {
 public:
     int longestCommonSubsequence(string s1, string s2) {
         int m=s1.size();
-        int n=s2.size(); 
+        int n=s2.size();
 
-        vector<vector<int>> t(m+1, vector<int> (n+1));
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
 
-        for(int row=0;row<m+1;row++) t[row][0]=0;
+         for(int i=0;i<m;i++) dp[i][0]=0;
+         for(int j=0;j<n;j++) dp[0][j]=0;
 
-        for(int col=0;col<n+1;col++) t[0][col]=0;
-
-        for(int i=1; i<m+1;i++)
+        for(int i=1;i<=m;i++)
         {
-            for(int j=1;j<n+1;j++)
+            for(int j=1;j<=n;j++)
             {
+               
                 if(s1[i-1]==s2[j-1])
-                    t[i][j]=1+t[i-1][j-1];
-
-                else 
-                    t[i][j] = max(t[i-1][j],t[i][j-1]);
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else {
+                    dp[i][j]=max(dp[i-1][j] , dp[i][j-1]);
+                }
             }
         }
 
-        return t[m][n];
-
+        return  dp[m][n];
     }
 };
 
-// // method 1 recu+ memo
+
+
 // class Solution {
 // public:
-//  int n , m;
-//  int t[1001][1001];
+
+//     int n , m;
+//     int t[1001][1001];
 //     int solve(string& s1,string& s2,int i,int j)
 //     {
 //         if(i>=n || j>=m) 
@@ -44,8 +47,7 @@ public:
         
 //         if(s1[i]==s2[j] ) 
 //             return t[i][j] =1+solve(s1,s2,i+1,j+1);
-        
-
+    
 //         return t[i][j]=max(solve(s1,s2,i+1,j),solve(s1,s2,i,j+1));
 //     }
 //     int longestCommonSubsequence(string s1, string s2) {
@@ -55,6 +57,5 @@ public:
 //         memset(t,-1,sizeof(t));
 
 //         return solve(s1,s2,0,0);
-        
 //     }
-// }; 
+// };
