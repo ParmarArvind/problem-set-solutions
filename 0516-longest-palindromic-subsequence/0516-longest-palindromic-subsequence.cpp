eb5e5252abee8395c77b9985a38d98984c9cc81a
Arvind Parmar <arvind1057956@gmail.com>
2026-08-22 12:51:51 +0530

    Time: 114 ms (5.03%), Space: 76.1 MB (22.96%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,23 +1,51 @@
+// method 2 using LCS bottom up 
 class Solution {
-public: 
-    int n;
-    int dp[1001][1001];
-    int LCS(string &s1 ,string &s2 ,int i,int j)
-    {
-        if(i==n || j==n ) return 0;
-
-        if(dp[i][j] !=-1) return dp[i][j];
-
-        if(s1[i] ==s2[j]) return dp[i][j] =1+LCS(s1,s2,i+1,j+1);
-        // else 
-        return dp[i][j] =max( LCS(s1,s2,i,j+1) ,LCS(s1,s2,i+1,j));
-    }
+public:
     int longestPalindromeSubseq(string s1) {
-        n=s1.size();
+        int n=s1.size();
         string s2=s1;
-        memset(dp,-1,sizeof(dp));
+       
         reverse(s2.begin() ,s2.end());
-        
-        return LCS(s1,s2,0 ,0);
+        vector<vector<int>>dp(n+1,vector<int>(n+1,1));
+
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=n;j++)
+            {
+                if(i==0 || j==0)  dp[i][j]=0;
+                else if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+                else {
+                    dp[i][j]= max(dp[i-1][j] ,dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[n][n];
     }
 };
+
+
+// //  method -1 using LCS recu+memo tc=O(n^2) ,sc =o(n^2)
+// class Solution {
+// public: 
+//     int n;
+//     int dp[1001][1001];
+//     int LCS(string &s1 ,string &s2 ,int i,int j)
+//     {
+//         if(i==n || j==n ) return 0;
+
+//         if(dp[i][j] !=-1) return dp[i][j];
+
+//         if(s1[i] ==s2[j]) return dp[i][j] =1+LCS(s1,s2,i+1,j+1);
+//         // else 
+//         return dp[i][j] =max( LCS(s1,s2,i,j+1) ,LCS(s1,s2,i+1,j));
+//     }
+//     int longestPalindromeSubseq(string s1) {
+//         n=s1.size();
+//         string s2=s1;
+//         memset(dp,-1,sizeof(dp));
+//         reverse(s2.begin() ,s2.end());
+        
+//         return LCS(s1,s2,0 ,0);
+//     }
+// };
