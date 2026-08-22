d4e0d209df4c11abad38bcc3bc615bd803b3ecfc
Arvind Parmar <arvind1057956@gmail.com>
2026-08-22 11:57:21 +0530

    Time: 4 ms (68.22%), Space: 8.3 MB (99.36%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,33 +1,63 @@
+// method -4 tc=O(n^2) sc=o(1)
 class Solution {
 public:
-    int countSubstrings(string s) {
-        int n=s.size();
-        vector<vector<bool>>dp(n,vector<bool>(n,false));
-        //state dp[i][j] =true ->s[i:j] is palindroome sustring both i and j inclusive
-
+    int n;
+    int check(int i,int j,string &s)
+    {
         int count=0;
-        for(int L=1;L<=n;L++)
+        while(i>=0 && j<n && s[i]==s[j])
         {
-            for(int i=0; L+i-1 < n ;i++)
-            {
-                int j=L+i-1;
-                if(i==j) dp[i][j]=true;
-                else if (i+1==j) 
-                {
-                    dp[i][j]= (s[i]==s[j]);
-                }
-                else {
-                    dp[i][j]=(dp[i+1][j-1] && s[i]==s[j]);
-                }
+            count++;
+            i-- , j++;
+        }
 
-                // count increace if current substring is palindrome
-                if(dp[i][j]==true) count++;
-            }
+        return count;
+    }
+    int countSubstrings(string s) {
+        n=s.size();
+        int count=0;
+        for(int i=0;i<n;i++)
+        {
+            count += check(i,i,s);// take ith char as mid and make a odd length palindrome substring
+            count += check(i,i+1,s); // take ith and (i+1 )th char as mid and make a even length palindrome substring
+            
         }
 
         return count;
     }
 };
+
+// /// method -3 bottom up  tc=O(n^2) ,sc=O(n^2)
+// class Solution { 
+// public:
+//     int countSubstrings(string s) {
+//         int n=s.size();
+//         vector<vector<bool>>dp(n,vector<bool>(n,false));
+//         //state dp[i][j] =true ->s[i:j] is palindroome sustring both i and j inclusive
+
+//         int count=0;
+//         for(int L=1;L<=n;L++)
+//         {
+//             for(int i=0; L+i-1 < n ;i++)
+//             {
+//                 int j=L+i-1;
+//                 if(i==j) dp[i][j]=true;
+//                 else if (i+1==j) 
+//                 {
+//                     dp[i][j]= (s[i]==s[j]);
+//                 }
+//                 else {
+//                     dp[i][j]=(dp[i+1][j-1] && s[i]==s[j]);
+//                 }
+
+//                 // count increace if current substring is palindrome
+//                 if(dp[i][j]==true) count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// };
 
 
 
