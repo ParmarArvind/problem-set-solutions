8ba9f7f0d283efa6c276d981b3a8f1480a6ffa2f
Arvind Parmar <arvind1057956@gmail.com>
2026-08-22 13:40:12 +0530

    Time: 19 ms (99.4%), Space: 13.1 MB (86.74%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,28 +1,50 @@
-// method 2 using LCS bottom up 
+// method -3 recursion +memo
 class Solution {
 public:
-    int longestPalindromeSubseq(string s1) {
-        int n=s1.size();
-        string s2=s1;
-       
-        reverse(s2.begin() ,s2.end());
-        vector<vector<int>>dp(n+1,vector<int>(n+1,1));
+    int dp[1001][1001];
+    int solve(string& s ,int i,int j)
+    {
+        if(i>j) return 0;
+        else if(i==j) return 1;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s[i]==s[j]) return dp[i][j]= 2+solve(s,i+1,j-1);
 
-        for(int i=0;i<=n;i++)
-        {
-            for(int j=0;j<=n;j++)
-            {
-                if(i==0 || j==0)  dp[i][j]=0;
-                else if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
-                else {
-                    dp[i][j]= max(dp[i-1][j] ,dp[i][j-1]);
-                }
-            }
-        }
-
-        return dp[n][n];
+        return dp[i][j]=max(solve(s,i+1,j) , solve(s,i,j-1));
+    }
+    int longestPalindromeSubseq(string s) {
+        int n=s.size();
+        memset(dp,-1, sizeof(dp));
+        return solve(s,0,n-1);
+        
     }
 };
+
+// // method 2 using LCS bottom up 
+// class Solution {
+// public:
+//     int longestPalindromeSubseq(string s1) {
+//         int n=s1.size();
+//         string s2=s1;
+       
+//         reverse(s2.begin() ,s2.end());
+//         vector<vector<int>>dp(n+1,vector<int>(n+1,1));
+
+//         for(int i=0;i<=n;i++)
+//         {
+//             for(int j=0;j<=n;j++)
+//             {
+//                 if(i==0 || j==0)  dp[i][j]=0;
+//                 else if(s1[i-1]==s2[j-1]) dp[i][j]=1+dp[i-1][j-1];
+//                 else {
+//                     dp[i][j]= max(dp[i-1][j] ,dp[i][j-1]);
+//                 }
+//             }
+//         }
+
+//         return dp[n][n];
+//     }
+// };
 
 
 // //  method -1 using LCS recu+memo tc=O(n^2) ,sc =o(n^2)
