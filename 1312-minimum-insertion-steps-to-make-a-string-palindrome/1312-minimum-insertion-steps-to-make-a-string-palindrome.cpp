b80ae00aa48ccca87337dec2bfe63e0a86b49d23
Arvind Parmar <arvind1057956@gmail.com>
2026-08-22 14:20:11 +0530

    Time: 23 ms (78.43%), Space: 31.3 MB (72.05%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,24 +1,50 @@
-
-// method -3 recursion +memo
+// method -4 bottom up
 class Solution {
 public:
-    int dp[1001][1001];
-    int solve(string& s ,int i,int j)
-    {
-        if(i>=j) return 0;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i]==s[j]) return dp[i][j]= solve(s,i+1,j-1);
-
-        return dp[i][j]=1+min(solve(s,i+1,j) , solve(s,i,j-1));
-    }
     int minInsertions(string s) {
         int n=s.size();
-        memset(dp,-1, sizeof(dp));
-        return solve(s,0,n-1);
         
+        vector<vector<int>>dp(n,vector<int>(n));
+
+        for(int i=0;i<n;i++) dp[i][i]=0;
+
+        for(int L=2;L<=n;L++)
+        {
+            for(int i=0;i+L-1<n;i++)
+            {
+                int j=i+L-1;
+                
+                if(s[i]==s[j]) dp[i][j]=dp[i+1][j-1];
+                else {
+                    dp[i][j]= 1+min(dp[i+1][j] ,dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
     }
 };
+
+// // method -3 recursion +memo
+// class Solution {
+// public:
+//     int dp[1001][1001];
+//     int solve(string& s ,int i,int j)
+//     {
+//         if(i>=j) return 0;
+        
+//         if(dp[i][j] != -1) return dp[i][j];
+//         if(s[i]==s[j]) return dp[i][j]= solve(s,i+1,j-1);
+
+//         return dp[i][j]=1+min(solve(s,i+1,j) , solve(s,i,j-1));
+//     }
+//     int minInsertions(string s) {
+//         int n=s.size();
+//         memset(dp,-1, sizeof(dp));
+//         return solve(s,0,n-1);
+        
+//     }
+// };
 
 
 // // method 2 using LCS bottom up 
