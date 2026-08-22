760f162d265ab056363d14aed95c4c03d819ada6
Arvind Parmar <arvind1057956@gmail.com>
2026-08-22 12:03:47 +0530

    Time: 178 ms (21.13%), Space: 18.2 MB (36.92%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,6 +1,7 @@
+// method -2 recu +memo tc= O(n^2)  ,sc=(n^2)
 class Solution {
 public:
-    // O(n^2) time
+    
     int t[1001][1001];
     bool isValid(int i, int j, string& s) {
         if (i >= j)
@@ -16,16 +17,51 @@ public:
         string result;
         memset(t, -1, sizeof(t));
 
-        for (int i = 0; i < s.size(); i++) {
-            string temp;
-            for (int j = i; j < s.size(); j++) {
-                temp.push_back(s[j]);
+        for (int i = 0; i < s.size(); i++)
+        {
+            for (int j = i; j < s.size(); j++) 
+            {
                 if (isValid(i, j, s)) {
                     if (result.size() < (j - i + 1))
-                        result = temp;
+                        result = s.substr(i,j-i+1);
                 }
             }
         }
         return result;
     }
 };
+
+// // brute force tc=O(n^3)
+// class Solution {
+// public:
+
+//     bool isValid(int i,int j,string &s)
+//     {
+//         while(i <= j)
+//         {
+//             if(s[i] != s[j])
+//             return false;
+//             i++;
+//             j--;
+//         }
+//         return true;
+//     }
+//     string longestPalindrome(string s) {
+//         string result;
+
+//         for(int i=0; i<s.size();i++)
+//         {
+//             string temp;
+//             for(int  j=i;j<s.size();j++)
+//             {
+//                 temp.push_back(s[j]);
+//                 if( isValid(i,j,s) )
+//                 {
+//                     if(result.size() < (j-i+1) ) result=temp;
+//                 }
+//             }
+//         }
+//         return result;
+        
+//     }
+// };
