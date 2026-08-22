76778d974334a8ab3efe92e9ec30791b2802d7a1
Arvind Parmar <arvind1057956@gmail.com>
2026-08-22 11:44:29 +0530

    Time: 31 ms (31.33%), Space: 11.1 MB (46.21%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,35 +1,67 @@
-
-// method -2  recursion and memo TC=O(n^2)
 class Solution {
 public:
-    int n;
-    int dp[1001][1001];
-    bool isPalindrome(string & s ,int i ,int j)
-    {
-        if(i>=j) return true;
-        
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(s[i] != s[j] )  return dp[i][j]=false;
-
-        return dp[i][j]=isPalindrome(s,i+1 ,j-1);
-    }
     int countSubstrings(string s) {
-        n=s.size();
-        int count=0;
-        memset(dp ,-1, sizeof(dp));
+        int n=s.size();
+        vector<vector<bool>>dp(n,vector<bool>(n,false));
+        //state dp[i][j] =true ->s[i:j] is palindroome sustring both i and j inclusive
 
-        for(int i=0;i<n;i++)
+        int count=0;
+        for(int L=1;L<=n;L++)
         {
-            for(int j=i;j<n;j++)
+            for(int i=0; L+i-1 < n ;i++)
             {
-                if(isPalindrome(s,i,j)) count++;
+                int j=L+i-1;
+                if(i==j) dp[i][j]=true;
+                else if (i+1==j) 
+                {
+                    dp[i][j]= (s[i]==s[j]);
+                }
+                else {
+                    dp[i][j]=(dp[i+1][j-1] && s[i]==s[j]);
+                }
+
+                // count increace if current substring is palindrome
+                if(dp[i][j]==true) count++;
             }
         }
 
         return count;
     }
 };
+
+
+
+// // method -2  recursion and memo TC=O(n^2)
+// class Solution {
+// public:
+//     int n;
+//     int dp[1001][1001];
+//     bool isPalindrome(string & s ,int i ,int j)
+//     {
+//         if(i>=j) return true;
+        
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         if(s[i] != s[j] )  return dp[i][j]=false;
+
+//         return dp[i][j]=isPalindrome(s,i+1 ,j-1);
+//     }
+//     int countSubstrings(string s) {
+//         n=s.size();
+//         int count=0;
+//         memset(dp ,-1, sizeof(dp));
+
+//         for(int i=0;i<n;i++)
+//         {
+//             for(int j=i;j<n;j++)
+//             {
+//                 if(isPalindrome(s,i,j)) count++;
+//             }
+//         }
+
+//         return count;
+//     }
+// };
 
 
 
