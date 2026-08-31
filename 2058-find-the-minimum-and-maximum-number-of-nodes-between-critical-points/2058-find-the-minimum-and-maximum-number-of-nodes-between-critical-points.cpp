ad94c4e5d0372d45378dbbeb88629fc6bd1a255f
Arvind Parmar <arvind1057956@gmail.com>
2026-08-31 12:54:45 +0530

    Time: 8 ms (26.86%), Space: 133.8 MB (5.02%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int>arr;
+        ListNode* temp=head;
+
+        while(temp)
+        {
+            arr.push_back(temp->val);
+            temp=temp->next;
+        }
+
+        int minIdx=-1;
+        int maxIdx=-1;
+        int prevIdx=-1;
+        vector<int>result={INT_MAX , INT_MIN};
+        for(int i=1;i<arr.size()-1;i++)
+        {
+            if((arr[i] > arr[i-1] && arr[i] > arr[i+1]) || 
+                (arr[i] < arr[i-1] && arr[i] < arr[i+1]) )
+            {
+                // calculate min distance
+                if(prevIdx !=-1){
+                    result[0]=min(result[0] ,i-prevIdx);
+                }
+                
+                prevIdx=i;
+
+                // update the minIdx , maxIdx and prevIdx
+                if(minIdx==-1){
+                    minIdx=i;
+                }
+                else {
+                    maxIdx=i;
+                }
+
+                // calculate maximum distance
+                if(minIdx != -1 && maxIdx !=-1 ) 
+                {
+                    result[1]=max(result[1] ,maxIdx-minIdx);
+                }
+                
+            }
+        }
+
+        if(minIdx ==-1 || prevIdx ==-1 || maxIdx ==-1) return {-1,-1};
+        return result;
+    }
+};
